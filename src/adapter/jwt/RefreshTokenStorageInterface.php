94870c857aa3783212508c4a9844bcf93c663020
Andrey <inbox.shelamkoff@gmail.com>
2020-10-13 20:39:58 +0300

    Create RefreshTokenStorageInterface.php

---
+++ src/adapter/jwt/RefreshTokenStorageInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Bermuda\Authentication\Adapter\Jwt;
+
+
+/**
+ * Interface RefreshTokenStorage
+ * @package Bermuda\Authentication\Adapter\Jwt
+ */
+interface RefreshTokenStorageInterface
+{
+    /**
+     * @param string $refreshToken
+     * @return bool
+     */
+    public function hasToken(string $refreshToken): bool ;
+
+    /**
+     * @param string $refreshToken
+     */
+    public function storeToken(string $refreshToken): void ;
+
+    /**
+     * @param string $refreshToken
+     * @return string
+     */
+    public function getUserIdentityFromRefreshToken(string $refreshToken): string ;
+}
