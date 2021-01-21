cc38a03603cd6c180925fe069b32fab365ba1d5e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-21 22:12:22 +0300

    Update AuthenticationMiddleware.php

--- src/AuthenticationMiddleware.php
+++ src/AuthenticationMiddleware.php
@@ -17,10 +17,12 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class AuthenticationMiddleware implements MiddlewareInterface
 {
     private AdapterInterface $adapter;
+    private ?SessionRepositoryInterface $repository;
 
-    public function __construct(AdapterInterface $adapter)
+    public function __construct(AdapterInterface $adapter, ?SessionRepository $repository = null)
     {
         $this->adapter = $adapter;
+        $this->repository = $repository;
     }
 
     /**
@@ -28,6 +30,38 @@ final class AuthenticationMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        return $this->adapter->write($request = $this->adapter->authenticate($request), $handler->handle($request));
+        $request = $this->adapter->authenticate($request);
+        $result  = $this->getResultFromRequest($request);
+        
+        if ($result->isAuthorized() && ($user = $result->getUser()) 
+            instanceof SessionAwareInterface)
+        {
+            if ($this->repository == null)
+            {
+                throw new \RuntimeException(SessionRepositoryInterface::class . ' instance is null');
+            }
+            
+            if (($id = $this->getIdFromRequest($request)) != null && 
+                ($session = $user->sessions()->get($id)) != null)
+            {
+                $session->activity(($this->dateTimeFactory)());
+            }
+            
+            else
+            {
+                $user->sessions()->add($session = $this->repository->make($user, $request));
+                $user->sessions()->setCurrentId($session->getId());
+            }
+            
+            $this->repository->store($session);
+            
+        }
+        
+        return $this->adapter->write($request, $handler->handle($request));
+    }
+    
+    private function getResultFromRequest(ServerRequestInterface $req): Result
+    {
+        return $request->getAttribute(AdapterInterface::resultAt);
     }
 }
