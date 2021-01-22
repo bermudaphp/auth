eae3209a554269f91e9a3635471ea58474469ffe
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-22 22:46:38 +0300

    Update AdapterFactory.php

--- src/AdapterFactory.php
+++ src/AdapterFactory.php
@@ -2,29 +2,18 @@
 
 namespace Bermuda\Authentication;
 
+use function Bermuda\urlFor;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Bermuda\Authentication\Adapter\PasswordAdapter;
 use Bermuda\Authentication\Adapter\CookieAdapter;
-use function Bermuda\redirect_on_route;
 
 final class AdapterFactory
 {
-    public const container_config_id = 'auth';
-    public const container_delegate_id = 'auth.delegate';
-    public const container_response_generator_id = 'auth.response_generator';
+    public const configID = 'AdapterFactory:configID';
     
     public function __invoke(ContainerInterface $container): CookieAdapter
-    {
-        $generator = $container->has(self::container_response_generator_id) ? $container->get(self::container_response_generator_id)
-            : static function(): ResponseInterface { return redirect_on_route('login'); };
-        
-        $config = $container->get('config')[self::container_config_id] ?? [];
-        $repository = $container->has(SessionRepositoryInterface::class) ? $container->get(SessionRepositoryInterface::class) : null;
-        
-        $delegate = $container->has(self::container_delegate_id) ? $container->get(self::container_delegate_id) 
-            : new PasswordAdapter($container->get(UserProviderInterface::class), $generator, $config, $repository);
-        
-        return new CookieAdapter($delegate->provider(), $generator, $config, $repository, $delegate);
+    {        
+        return new CookieAdapter($container->get(self::configID));
     }
 }
