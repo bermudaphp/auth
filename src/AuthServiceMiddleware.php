725ecc8e3d440db38285461f9ee2d8d7f344dde5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-19 16:56:12 +0300

    Update AuthServiceMiddleware.php

--- src/AuthServiceMiddleware.php
+++ src/AuthServiceMiddleware.php
@@ -18,12 +18,18 @@ final class AuthServiceMiddleware implements MiddlewareInterface
     private static bool $remember = false;
     private static ?UserInterface $user = null;
 
-    private ?SessionRepositoryInterface $repository;
+    private ?SessionStorageInterface $storage;
+    
+    public const user_at = 'Bermuda\Authentication\AuthServiceMiddleware@user_at';
 
-    public function __construct(AdapterInterface $adapter, ?SessionRepositoryInterface $repository = null)
+    public function __construct(AdapterInterface $adapter, ?SessionStorageInterface $storage = null)
     {
-        $this->adapter = $adapter;
-        $this->repository = $repository;
+        $this->adapter = $adapter; $this->storage = $storage;
+    }
+    
+    public function storage(?SessionStorageInterface $storage = null):? SessionStorageInterface
+    {
+        return $storage ? $this->storage = $storage : $this->storage;
     }
 
     /**
@@ -31,94 +37,25 @@ final class AuthServiceMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $response = $handler->handle($request = $this->authenticate($request));
-
-        if (self::isAuthenticated())
+        if (($result = $this->authenticate($request))->isAuthorized())
         {
-            $request = $this->authenticate($request, self::$user, self::$remember);
-        }
-
-        return $this->adapter->write($request, $response);
-    }
-    
-    public function repository(?SessionRepositoryInterface $repository = null):? SessionRepositoryInterface
-    {
-        if ($repository != null)
-        {
-            $this->repository = $repository;
-        }
-        
-        return $this->repository;
-    }
-
-    /**
-     * @param UserInterface $user
-     * @param bool $remember
-     */
-    public static function auth(UserInterface $user, bool $remember = false): void
-    {
-        self::$user = $user; self::$remember = $remember;
-    }
-    
-    public static function isAuthenticated(): bool
-    {
-        return self::$user != null;
-    }
-    
-    /**
-     * @param UserInterface $user
-     * @param bool $remember
-     */
-    public static function logout(): void
-    {
-        self::$user = null; self::$remember = null;
-    }
-    
-    public static function user():? UserInterface
-    {
-        return self::$user;
-    }
-
-    /**
-     * @param ServerRequestInterface $request
-     * @param UserInterface|null $user
-     * @param bool $remember
-     * @return ServerRequestInterface
-     */
-    private function authenticate(ServerRequestInterface $request, ?UserInterface $user = null, bool $remember = false): ServerRequestInterface
-    {
-        $request = $this->adapter->authenticate($request, $user, $remember);
-        $result  = $this->getResultFromRequest($request);
-        
-        if ($result->isAuthorized() && ($user = $result->getUser()) 
-            instanceof SessionAwareInterface)
-        {
-            if ($this->repository == null)
-            {
-                throw new \RuntimeException('Bermuda\Authentication\SessionRepositoryInterface instance not set. Call '. __CLASS__ . '::repository()');
-            }
+            $request = $request->withAttribute(self::user_at, $user = $result->getUser());
             
-            if (($session = $user->sessions()->current()) != null)
+            if ($user instanceof SessionAwareInterface)
             {
-                $session->activity($this->getCurrentTime());
-                $this->repository->store($session);
+                if ($this->storage == null)
+                {
+                    throw new \RuntimeException('Bermuda\Authentication\SessionStorageInterface instance not set. Call '. __CLASS__ . '::storage()');
+                }
+            
+                if (($session = $user->sessions()->current()) != null)
+                {
+                    $session->activity(new \DateTimeImmutable());
+                    $this->storage->store($session);
+                }
             }
         }
-        
-        return $request;
-    }
 
-    /**
-     * @param ServerRequestInterface $req
-     * @return Result
-     */
-    private function getResultFromRequest(ServerRequestInterface $req): Result
-    {
-        return $req->getAttribute(AdapterInterface::resultAt);
-    }
-    
-    private function getCurrentTime(): \DateTimeInterface
-    {
-        return new \DateTimeImmutable();
+        return $this->adapter->write($response = $handler->handle($request), $response);
     }
 }
