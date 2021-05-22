1dbed105023f96dd4504623fa9ea1fb3cf4d7b9f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-22 14:12:37 +0300

    Update AggregateAdapter.php

--- src/adapter/AggregateAdapter.php
+++ src/adapter/AggregateAdapter.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * Class AggregateAdapter
+ * Class PasswordAdapter
  * @package Bermuda\Authentication\Adapter
  */
 final class PasswordAdapter extends AbstractAdapter
