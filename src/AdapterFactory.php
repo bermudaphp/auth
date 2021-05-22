5ef01d4e26cef5296bfa81a86047328249140392
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-22 14:25:44 +0300

    Update AdapterFactory.php

--- src/AdapterFactory.php
+++ src/AdapterFactory.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 final class AdapterFactory
 {    
     public const cookie_params_id = 'Bermuda\Authentication\AdapterFactory@cookie_params';
-    public function __invoke(ContainerInterface $container): Adapter\CookieAdapter
+    public function __invoke(ContainerInterface $container): Adapter\PasswordAdapter
     {         
         ($adapter = new Adapter\PasswordAdapter($provider = $container->get(UserProviderInterface::class), $generator = static fn(): ResponseInterface => view('app::login')))
             ->setNext(new Adapter\CookieAdapter($provider, $generator, $container->get(self::cookie_params_id)));
